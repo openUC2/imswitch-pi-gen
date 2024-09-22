3231f21b7e1a229b198d984aeaa708ecc51a6c38
beniroquai <bene.d@gmx.de>
2024-09-22 08:42:31 +0200

    This runs in non-docker mode

--- stage2a/03-pull-docker/00-run.sh
+++ stage2a/03-pull-docker/00-run.sh
@@ -1,24 +1,6 @@
 #!/bin/bash -e
 
-export PATH=$PATH:/usr/bin:/usr/local/bin
-
-
-#!/bin/bash -e
-
-# Ensure Docker is installed
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
-
-# Create a directory for saving the Docker image inside the image
-mkdir -p /home/uc2/docker_images
-
-# Download the Docker image and save it as a tar file
+# Pull the Docker image from GitHub Container Registry
 echo "Pulling Docker image from GitHub Container Registry"
-docker pull ghcr.io/openuc2/imswitch-noqt-x64:latest
-docker save -o /home/uc2/docker_images/imswitch-noqt-x64.tar ghcr.io/openuc2/imswitch-noqt-x64:latest
+#docker pull ghcr.io/openuc2/imswitch-noqt-x64:latest
 # docker run -it --rm -p 8001:8001 -p 2222:22 -e HEADLESS=1 -e HTTP_PORT=8001 -e CONFIG_FILE=example_virtual_microscope.json -e UPDATE_GIT=0 -e UPDATE_CONFIG=0 --privileged ghcr.io/openuc2/imswitch-noqt-x64:latest
-
-# Set appropriate permissions for the tar file
-chmod 644 /home/uc2/docker_images/imswitch-noqt-x64.tar
-
-
--- stage2a/04-avahi-service/01-run.sh
+++ stage2a/04-avahi-service/01-run.sh
@@ -3,4 +3,4 @@ This script registers an avahi service to broadcast the device as being
 an available ImSwitch Microscope. This can be used for discovery of
 microscopes connected to the network.
 '
-install -v -m 644 files/ofm.service "${ROOTFS_DIR}/etc/avahi/services/imswitch.service"
+install -v -m 644 files/imswitch.service "${ROOTFS_DIR}/etc/avahi/services/imswitch.service"
